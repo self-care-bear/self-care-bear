3f0ef442318e0bc027ca868cbc2a0ad8baeb00eb
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-28 15:23:59 -0800

    updated package.json

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react';
-import { getProfile } from '../../services/profiles';
+// import { getProfile } from '../../services/profiles';
 import { getPresetTasks } from '../../services/tasks';
 
 export default function Profile() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getPresetTasks();
-      console.log(data);
     };
     fetchData();
   }, []);
