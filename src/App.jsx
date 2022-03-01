ce8f12eebd245214ae1193176f66cd55792507a5
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-01 10:22:37 -0800

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './views/Layout/Layout';
 import Home from './views/Home/Home';
 import About from './views/About/About';
-import TaskSelector from './views/Task/TaskSelector';
+import TaskSelector from './views/Tasks/TaskSelector';
+import CreateProfile from './views/Profile/CreateProfile';
+import TaskDetail from './views/Tasks/TaskDetail';
+import Completed from './views/Completed/Completed';
 
 export default function App() {
   return (
@@ -24,17 +27,23 @@ export default function App() {
               <Auth />
             </Route>
             <Route exact path="/profile/create">
-              {/* need to make */}
+              <CreateProfile />
             </Route>
             <Route path="/profile/tasks">
               <TaskSelector />
             </Route>
-            <Route path="/profile/tasks/:id">{/* need to make */}</Route>
-            <Route path="/profile/tasks/:id/edit">{/* need to make */}</Route>
+            <Route path="/profile/tasks/:id">
+              <TaskDetail />
+            </Route>
+            <Route path="/profile/tasks/:id/edit">
+              <TaskDetail isEditing />
+            </Route>
             <Route exact path="/profile">
               <Profile />
             </Route>
-            <Route path="/profile/completed">{/* need to make */}</Route>
+            <Route path="/profile/completed">
+              <Completed />
+            </Route>
             <Route path="/about">
               <About />
             </Route>
