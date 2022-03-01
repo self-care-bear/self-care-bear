e54f376ee6b0fef2c2540ce0e5de2a3bc446fc33
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-02-28 16:08:52 -0800

    added in routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,41 @@ import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
 import Profile from './views/Profile/Profile';
 import Auth from './views/Auth/Auth';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from './views/Layout/Layout';
+import Home from './views/Home/Home';
+import About from './views/About/About';
 export default function App() {
   return (
     <>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/signup">
+              <Auth isSigningUp />
+            </Route>
+            <Route path="/signin">
+              <Auth />
+            </Route>
+            <Route exact path="/profile/create">
+              {/* need to make */}
+            </Route>
+            <Route path="/profile/tasks">{/* need to make */}</Route>
+            <Route path="/profile/tasks/:id">{/* need to make */}</Route>
+            <Route path="/profile/tasks/:id/edit">{/* need to make */}</Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/profile/completed">{/* need to make */}</Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Layout>
+      </BrowserRouter>
       <h1
         className={`
       bg-green-400
@@ -22,8 +53,6 @@ export default function App() {
       >
         Hello, World!
       </h1>
-      <Profile />
-      <Auth isSigningUp={true} />
     </>
   );
 }
