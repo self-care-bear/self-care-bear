ca4d66ffbad0b4d93b89be1187b4dc1e7a0744d0
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-01 14:22:51 -0800

    added updateTask function

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -24,7 +24,13 @@ export async function createTask(task, task_description) {
   return parseData(request);
 }
 
-export async function updateTask(task, task_description) {}
+export async function updateTask(task, task_description) {
+  const request = await client
+    .from('created_tasks')
+    .update({ task, task_description })
+    .match({ user_id });
+  return parseData(request);
+}
 
 export async function deleteTask(id) {
   const request = await client.from('created_tasks').delete().match({ id });
