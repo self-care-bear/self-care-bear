f18509c2a5cc2c0c22de4dad3d309609578b2de6
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-01 15:00:08 -0800

    updateTask function working

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -24,7 +24,7 @@ export async function createTask(task, task_description) {
   return parseData(request);
 }
 
-export async function updateTask(task, task_description) {
+export async function updateTask(task, task_description, user_id) {
   const request = await client
     .from('created_tasks')
     .update({ task, task_description })
