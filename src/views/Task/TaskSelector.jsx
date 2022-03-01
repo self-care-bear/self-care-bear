784dda4db82f85fa89400f0548e7fdd18a9a7741
Emma Egstad <emma@egstad.com>
2022-03-01 09:42:04 -0800

    populating task list with created and preset tasks

--- src/views/Task/TaskSelector.jsx
+++ src/views/Task/TaskSelector.jsx
@@ -1,39 +1,34 @@
 import { getCreatedTasks, getPresetTasks } from '../../services/tasks';
 import { useEffect, useState } from 'react';
 import { useUser } from '../../context/UserContext';
+import { v4 as uuid } from 'uuid';
 
 export default function TaskSelector() {
   const { user } = useUser();
-  const [array1, setArray1] = useState([]);
-  const [array2, setArray2] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [taskList, setTaskList] = useState([]);
 
   useEffect(() => {
     const fetchCreatedData = async () => {
       const createdData = await getCreatedTasks(user.id);
-      console.log('createdData', createdData);
-      setArray1(createdData);
-      console.log('array1', array1);
+      setTaskList((prevState) => [...prevState, ...createdData]);
     };
     const fetchPresetData = async () => {
       const presetData = await getPresetTasks();
-      console.log('presetData', presetData);
-      setArray2(presetData);
-      console.log('array2', array2);
+      setTaskList((prevState) => [...prevState, ...presetData]);
+      setLoading(false);
     };
     fetchCreatedData();
     fetchPresetData();
-    setTaskList([...array1, ...array2]);
-    console.log('taskList', taskList);
-  }, [user]);
+  }, []);
 
-  // TUESDAY! fix this! get that taskList to work!
-  // taskList gets returned if array1 & array 2 are dependencies
-  // but it also gets stuck in an infinite loop?
+  if (loading) return <span>Loading...</span>;
 
   return (
     <div>
-      <h1>Hello, {user.email}!</h1>
+      {taskList.map((task) => {
+        return <p key={uuid()}>{task.task}</p>;
+      })}
     </div>
   );
 }
