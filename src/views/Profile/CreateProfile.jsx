daed1388434d024eb68dd9c455121e31effeeb80
Ian <84160879+ian-ch-jsx@users.noreply.github.com>
2022-03-01 17:26:55 -0800

    create profile and conditional profile page render (#15)
    * added profile creation page
    
    * user profiles saving with corresponding user id
    
    * conditional profile redirection

--- src/views/Profile/CreateProfile.jsx
+++ src/views/Profile/CreateProfile.jsx
@@ -1,5 +1,70 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useProfile } from '../../hooks/useProfile';
+import { useUser } from '../../context/UserContext';
+import BrownBear from '../../assets/brown2.png';
+import PandaBear from '../../assets/panda2.png';
+import './Profile.css';
+import { createProfile } from '../../services/profiles';
 
 export default function CreateProfile() {
-  return <div>CreateProfile</div>;
+  const { user } = useUser();
+  const { profile, setProfile, loading } = useProfile();
+  const history = useHistory();
+  const [bear, setBear] = useState('');
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setName(profile.user_name);
+    setBear(profile.bear);
+  }, [profile]);
+
+  const handleChange = (e) => {
+    setBear(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await createProfile({
+      user_id: user.id,
+      user_name: name,
+      bear: bear,
+      task_list: '',
+    });
+    setProfile(response);
+  };
+  return (
+    <div className="create-container">
+      <h2>create profile</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>
+          <input
+            type="radio"
+            name="bear"
+            value="brown"
+            checked={bear === 'brown'}
+            onChange={handleChange}
+          />
+          <img src={BrownBear} />
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="bear"
+            value="panda"
+            checked={bear === 'panda'}
+            onChange={handleChange}
+          />
+          <img src={PandaBear} />
+        </label>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,14 +1,27 @@
 import { useEffect } from 'react';
-// import { getProfile } from '../../services/profiles';
-import { getPresetTasks } from '../../services/tasks';
+import { Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+import { useProfile } from '../../hooks/useProfile';
+import { getProfileById } from '../../services/profiles';
+import CreateProfile from './CreateProfile';
 
 export default function Profile() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getPresetTasks();
-    };
-    fetchData();
-  }, []);
+  const { user } = useUser();
+  const { profile, setProfile, loading } = useProfile();
+  // useEffect(async () => {
+  //   try {
+  //     const response = await getProfileById(user.id);
+  //     setProfile(response);
+  //   } catch (error) {
+  //     <CreateProfile />;
+  //   }
+  // }, []);
+  if (loading) return <p>loading...</p>;
 
-  return <div>Profile</div>;
+  if (!loading && !profile.user_name) return <Redirect to="/profile/create" />;
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  );
 }
