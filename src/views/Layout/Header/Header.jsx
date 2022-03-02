0fce7de8508ebcef5e985fbb2e92c8b8095ec822
Libbi Dunham(she/her) <89104000+Libbi-Dunham@users.noreply.github.com>
2022-03-01 20:24:34 -0800

    Libbi2 (#18)
    * added history so redirected after created profile
    
    * trying to fix errors/moving around code
    
    * added a handleSelect for adding tasks

--- src/views/Layout/Header/Header.jsx
+++ src/views/Layout/Header/Header.jsx
@@ -1,13 +1,20 @@
 import { useUser } from '../../../context/UserContext';
+import { signOutUser } from '../../../services/auth';
+import { useHistory } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
+  const history = useHistory();
   const { user } = useUser();
+  const handleLogout = () => {
+    signOutUser();
+    history.replace('/');
+  };
   return (
     <header>
       <h1>Self Care Bear</h1>
       <h2>You're signed in as {user.email}</h2>
-      <button>log out</button>
+      <button onClick={handleLogout}>log out</button>
     </header>
   );
 }
