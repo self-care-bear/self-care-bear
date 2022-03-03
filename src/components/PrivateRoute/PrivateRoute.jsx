b03359d7890bfbbaf78a6a757bff8160e302e967
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-02 16:44:01 -0800

    added private routes

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+
+export default function PrivateRoute({ children, ...rest }) {
+  const { user } = useUser();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user.email ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
