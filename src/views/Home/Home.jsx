fd6db89deada50232f40e2c75cb2237ef7003c76
Emma Egstad <emma@egstad.com>
2022-03-03 09:16:58 -0800

    fixing text

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,14 +5,14 @@ import Bear from '../../assets/brown1.png';
 export default function Home() {
   return (
     <div className="home-container">
-      <h3>
+      <p>
         An app that cares about you and wants you to care for yourself, this
         site exists to help you create your ideal morning routine.
-      </h3>
-      <h3>
+      </p>
+      <p>
         Start your day off on the right foot with your Self Care Bear, and take
         care of your buddy like you’d take care of yourself.
-      </h3>
+      </p>
       <img className="home-container_image" src={Bear} alt="brown bear" />
       <div className="home-container_links">
         <Link to="/signin">Sign-in</Link>
