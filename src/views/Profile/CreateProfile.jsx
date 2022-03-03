95d25ead5fb8e5d06e243ea665d82d514d80803f
Emma Egstad <72117040+emmaegstad@users.noreply.github.com>
2022-03-03 15:12:39 -0800

    Updating styles on habit page (#34)
    * updating header styles
    
    * adding image to home page and links to auth
    
    * adding copy across the app and updating styles
    
    * fixing text
    
    * slight adjustments to home and auth
    
    * styling habit builder page
    
    * styling habit builder page again

--- src/views/Profile/CreateProfile.jsx
+++ src/views/Profile/CreateProfile.jsx
@@ -53,6 +53,7 @@ export default function CreateProfile() {
           placeholder="give your buddy a name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+          required
         />
         <div className="create-container_bears">
           <label>
