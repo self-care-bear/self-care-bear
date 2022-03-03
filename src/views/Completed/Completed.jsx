09aad4ad01f6333eeb5d36c4f84e24283f178c08
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-02 17:50:17 -0800

    pulled final bear out to the completed component

--- src/views/Completed/Completed.jsx
+++ src/views/Completed/Completed.jsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import './Completed.css';
+import panda6 from '../../assets/panda6.png';
+import brown6 from '../../assets/brown6.png';
+import { useProfile } from '../../hooks/useProfile';
 
 export default function Completed() {
-  return <div>Completed</div>;
+  const { profile } = useProfile();
+
+  return (
+    <div className="completed-container">
+      {(profile.bear === 'brown' && <img src={brown6} />) ||
+        (profile.bear === 'panda' && <img src={panda6} />)}
+    </div>
+  );
 }
