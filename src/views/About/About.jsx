b72a409064afb0943479a436342edcd8ead4afcf
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-03 11:55:30 -0800

    added some styling to about us

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -9,87 +9,99 @@ import github from '../../assets/Github/github.png';
 export default function About() {
   return (
     <div className="about">
-      <div>dis/ meet the creators</div>
-      <div>
-        <h4>
-          {' '}
-          <a href="https://www.linkedin.com/in/ianchjsx/" target="_blank">
-            Ian Christiansen
-          </a>
-        </h4>
-        <img src={ian} alt="ian headshot" />
-
-        <p>
-          Software developer transitioning from a background in wildlife
-          education and business management. As lifelong learner with a diverse
-          professional background, I thrive in collaborative environments where
-          unique perspectives can come together to solve complex problems. I
-          strive to continue to participate in socially responsible work through
-          advocacy, teaching, and learning - both professionally and personally.
-        </p>
-        <a href="https://github.com/ian-ch-jsx" target="_blank">
-          {' '}
-          <img src={github} alt="ian github" />
-        </a>
+      <div className="dis">
+        Meet the creators of Self Care Bear! A group of budding software
+        engineers who are passionate about self care. We recognize the
+        importance of spending time caring for ourselves so that we can care for
+        others and our code. We built this app with the goal of helping others
+        establish a morning routine. We hope you love it.
       </div>
-      <div>
-        <h4>
-          {' '}
-          <a href="https://www.linkedin.com/in/emmaegstad/" target="_blank">
+      <div className="bio-container">
+        <div className="bio-card">
+          <h4>
             {' '}
-            Emma Egstad
+            <a href="https://www.linkedin.com/in/ianchjsx/" target="_blank">
+              Ian Christiansen
+            </a>
+          </h4>
+          <a href="https://github.com/ian-ch-jsx" target="_blank">
+            {' '}
+            <img className="github" src={github} alt="ian github" />
           </a>
-        </h4>
-        <img src={emma} alt="emma headshot" />
-        <p>
-          Emma is a web developer in Portland, OR. Her favorite self-care
-          activity is reading a book under ten blankets on a rainy day. In
-          addition to reading and rain, Emma loves javascript, CSS, and React
-          testing (just kidding).
-        </p>
-        <a href="https://github.com/emmaegstad" target="_blank">
-          {' '}
-          <img src={github} alt="emma github" />
-        </a>
-      </div>
-      <div>
-        <h4>
-          {' '}
-          <a href="https://www.linkedin.com/in/libbi-dunham/" target="_blank">
-            Libbi Dunham
+          <img src={ian} alt="ian headshot" />
+
+          <p>
+            Software developer transitioning from a background in wildlife
+            education and business management. As lifelong learner with a
+            diverse professional background, I thrive in collaborative
+            environments where unique perspectives can come together to solve
+            complex problems. I strive to continue to participate in socially
+            responsible work through advocacy, teaching, and learning - both
+            professionally and personally.
+          </p>
+        </div>
+        <div className="bio-card">
+          <h4>
+            {' '}
+            <a href="https://www.linkedin.com/in/emmaegstad/" target="_blank">
+              {' '}
+              Emma Egstad
+            </a>
+          </h4>
+          <a href="https://github.com/emmaegstad" target="_blank">
+            {' '}
+            <img className="github" src={github} alt="emma github" />
           </a>
-        </h4>
-        <img src={libbi} alt="libbi headshot" />
-        <p>
-          I am a detailed oriented critical thinking Software Developer. I have
-          a background in customer service and administration which has given me
-          the skills with team collaboration, problem solving, communication,
-          and leadership. My next Software Developer role will allow me to use
-          these tools. I am a passionate learner, who enjoys learning new
-          technical knowledge.
-        </p>
-        <a href="https://github.com/Libbi-Dunham" target="_blank">
-          <img src={github} alt="libbi github" />
-        </a>
-      </div>
-      <div>
-        <h4>
-          {' '}
-          <a href="https://www.linkedin.com/in/michellenygren/" target="_blank">
-            Michelle Nygren
+          <img src={emma} alt="emma headshot" />
+          <p>
+            Emma is a web developer in Portland, OR. Her favorite self-care
+            activity is reading a book under ten blankets on a rainy day. In
+            addition to reading and rain, Emma loves javascript, CSS, and React
+            testing (just kidding).
+          </p>
+        </div>
+        <div className="bio-card">
+          <h4>
+            {' '}
+            <a href="https://www.linkedin.com/in/libbi-dunham/" target="_blank">
+              Libbi Dunham
+            </a>
+          </h4>
+          <a href="https://github.com/Libbi-Dunham" target="_blank">
+            <img className="github" src={github} alt="libbi github" />
           </a>
-        </h4>
-        <img src={michelle} alt="michelle headshot" />
-        <p>
-          Michelle is a software developer in Portland, OR. She tries to
-          prioritize self care in all she does, and especially focuses on
-          slowing down, being present, and taking weekly baths. In addition,
-          Michelle loves building apps that help build community and connection.
-          Oh, and React. She loves React.
-        </p>
-        <a href="https://github.com/michellerenehey" target="_blank">
-          <img src={github} alt="michelle github" />
-        </a>
+          <img src={libbi} alt="libbi headshot" />
+          <p>
+            I am a detailed oriented critical thinking Software Developer. I
+            have a background in customer service and administration which has
+            given me the skills with team collaboration, problem solving,
+            communication, and leadership. My next Software Developer role will
+            allow me to use these tools. I am a passionate learner, who enjoys
+            learning new technical knowledge.
+          </p>
+        </div>
+        <div className="bio-card">
+          <h4>
+            {' '}
+            <a
+              href="https://www.linkedin.com/in/michellenygren/"
+              target="_blank"
+            >
+              Michelle Nygren
+            </a>
+          </h4>
+          <a href="https://github.com/michellerenehey" target="_blank">
+            <img className="github" src={github} alt="michelle github" />
+          </a>
+          <img src={michelle} alt="michelle headshot" />
+          <p>
+            Michelle is a software developer in Portland, OR. She tries to
+            prioritize self care in all she does, and especially focuses on
+            slowing down, being present, and taking weekly baths. In addition,
+            Michelle loves building apps that help build community and
+            connection. Oh, and React. She loves React.
+          </p>
+        </div>
       </div>
     </div>
   );
