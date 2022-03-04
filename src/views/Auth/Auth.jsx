0f822d7c5bf336e626d194f772fbd12f1523f8b3
Emma Egstad <emma@egstad.com>
2022-03-03 16:33:51 -0800

    fixing context bug

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -16,12 +16,16 @@ export default function Auth({ isSigningUp = false }) {
     try {
       if (isSigningUp) {
         const response = await signUpUser(email, password);
-        setError(response.error.message);
+        if (response.error) {
+          setError(response.error.message);
+        }
         setUser({ id: response.user.id, email: response.user.email });
         history.replace('/profile/tasks');
       } else {
         const response = await signInUser(email, password);
-        setError(response.error.message);
+        if (response.error) {
+          setError(response.error.message);
+        }
         setUser({ id: response.user.id, email: response.user.email });
         history.replace('/profile/tasks');
       }
