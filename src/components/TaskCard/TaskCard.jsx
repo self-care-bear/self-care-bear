de84a8bf2b4ec04d963e8d03f264eb849274b84f
Ian Christiansen <ianc.songdog@gmail.com>
2022-04-08 11:06:00 -0700

    implementing grading refactor suggestions
    implemented changes per reccomendations
    tasklist filter is breaking the profile redirect logic
    counter broken
    task info does not populate immediately when new task is added to the list

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -4,13 +4,7 @@ import { deleteTask, updateTask } from '../../services/tasks';
 import { useTasks } from '../../context/TaskContext';
 import './TaskCard.css';
 
-export default function TaskCard({
-  task,
-  onEdit,
-  setIsSelected,
-  isSelected,
-  handleHabitsLeft,
-}) {
+export default function TaskCard({ task, onEdit, handleSelect }) {
   const { user } = useUser();
   const [isExpanded, setIsExpanded] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
@@ -32,21 +26,8 @@ export default function TaskCard({
     setIsEditing(false);
   };
 
-  const handleSelect = async () => {
-    await updateTask(task.id, task.task, task.task_description, user.id, true);
-    const taskSelected =
-      isSelected[task.id] !== undefined
-        ? !isSelected[task.id]
-        : !task.is_selected;
-    setIsSelected((prevState) => ({
-      ...prevState,
-      [task.id]: taskSelected,
-    }));
-    handleHabitsLeft();
-  };
-
   return (
-    <div className="task-card">
+    <div className={`task-card ${task.is_selected ? 'selected' : ''}`}>
       <h3 className="task-card_title">{task?.task}</h3>
       {isExpanded && <p>{task?.task_description}</p>}
       <div className="task-card_buttons">
@@ -56,7 +37,9 @@ export default function TaskCard({
             <button onClick={handleDelete}>Delete</button>
           </>
         )}
-        {!isEditing && <button onClick={handleSelect}>Set Habit</button>}
+        {!isEditing && (
+          <button onClick={() => handleSelect(task)}>Set Habit</button>
+        )}
       </div>
       {!isExpanded && (
         <p
